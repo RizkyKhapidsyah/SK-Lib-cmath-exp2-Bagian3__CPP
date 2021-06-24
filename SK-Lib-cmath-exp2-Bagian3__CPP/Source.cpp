e15cb37fd8c8c41033a2e22be58b738b1b770470
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-24 16:15:01 +0700

    Add project files.

---
+++ SK-Lib-cmath-exp2-Bagian3__CPP/Source.cpp
@@ -0,0 +1,21 @@
+// C++ program to illustrate the
+// exp2() function for positive numbers
+#include <cmath>
+#include <iostream>
+#include <conio.h>
+
+/*	Source by GeeksForGeeks
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019 */
+
+using namespace std;
+
+int main() {
+	int n = 6;
+
+	int ans = exp2(n);
+	cout << "exp2(6) = " << ans << endl;
+
+	_getch();
+	return 0;
+}
